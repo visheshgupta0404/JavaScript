173be692767def5b14a2a3c64ff026598af30b45
visheshgupta0404 <visheshg990@gmail.com>
2025-12-16 19:42:28 +0530

    This is variable usage of the Javascript

---
+++ script.js
@@ -0,0 +1,38 @@
+// console.log("Hello thi is the second day of learning")
+// Variables
+// var  
+// In others language  we use
+// DataType varName = value; -> Strongly typed language
+// but JS is a loose typed language  
+
+
+//var
+
+// var -> Global
+// can be reassign
+// Kahi bhi access karlo
+
+//let
+
+// let ->Local 
+// can be reassign
+// cant access outside the block of code of  initialization
+
+//const
+
+// cont->Local
+// can not be reassign
+// can not be access outside the code of initialization
+
+
+
+var age = 22
+console.log(age)
+age = "Vishesh"
+console.log(age)
+
+const ramesh = "Ramesh Gupta"
+console.log(ramesh)
+//  ramesh = "Suresh"  Gives Error
+
+
