a6c762ef6f08846b1ad44f1ece1f8964fb3cec5a
visheshgupta0404 <visheshg990@gmail.com>
2025-12-20 15:11:47 +0530

    Adding functions

--- 11_Arrays_Higher_Order_functions/script.js
+++ 11_Arrays_Higher_Order_functions/script.js
@@ -1,1 +1,29 @@
-console.log("Hey Everyone Today we are studying the Higher Order Functions in an Array")
+console.log(
+  "Hey Everyone Today we are studying the Higher Order Functions in an Array"
+);
+
+const randomNumbers = [1, 23, 42, 32, 48, 27, 91, 111];
+// Basically it is like others forEach loop in this it passes the value of each index to new function as value val we can directly use it or modify it and also it doesn't make changes in the Array
+randomNumbers.forEach((val) => console.log(val));
+randomNumbers.forEach((val) => console.log(val ** 2)); //It prints the Square of every number
+
+// .map is also  used as same but in this it passes the valves to the new Array
+// It uses when we want to  make new Array with changes
+
+let squareArray = randomNumbers.map((val) => val ** 2); //It passes the new value that we can store in the new Array
+
+console.log(squareArray);
+
+// .filter helps to filter out the Array bassed on the certain conditins
+
+let evenArray=randomNumbers.filter((val)=> val%2==0) //Passes new value that satisfied the given condition
+console.log(evenArray)
+let search = randomNumbers.find((num)=>num===42) //Passese the same valve if present if not then undefined 
+console.log(search)
+
+let searchindex = randomNumbers.findIndex((num)=> num===12) //passes  the index if value matches otherwise pass -1
+console.log(searchindex)
+
+const slicedArray = randomNumbers.slice(2,5)//slice the given Array from (start_index,end-1_index)
+console.log(slicedArray)
+
