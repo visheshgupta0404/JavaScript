5d334253abba55b11731a887b462726c7d7b0771
visheshgupta0404 <visheshg990@gmail.com>
2025-12-24 18:33:07 +0530

    Adding stopwatch files

---
+++ StopWatch/script.js
@@ -0,0 +1,25 @@
+let sec =0
+const timerButton = document.getElementById("timerButton")
+
+
+timerButton.addEventListener("click",()=>{
+    const stoptime = document.getElementById("userTime")
+    console.log(stoptime.value)
+    sec =stoptime.value
+    const count = document.getElementById("stopWatch")
+    count.innerText= sec
+
+    
+    const stop =setInterval(()=>{
+        count.innerText= sec-1
+        sec--
+    },1000)
+
+
+    setTimeout(()=>{
+        clearInterval(stop)
+        sec=0
+        count.innerText= sec
+    },(Number(stoptime.value)*1000)) 
+
+})
