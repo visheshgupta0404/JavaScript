75c2fefbccc038c9241e5dce7ec819435645d6d3
visheshgupta0404 <visheshg990@gmail.com>
2025-12-30 23:21:50 +0530

    Adding the new Iteratorfile

---
+++ 28_Iterator/script.js
@@ -0,0 +1,31 @@
+function makeIterator (start = 0 , noOFSteps = Infinity  , step = 1){
+    let nextStep  = start;
+    let iteratorCount = 0
+    return{
+        next(){
+        if(iteratorCount < noOFSteps){
+            let result =  {value : nextStep , done :false}
+            nextStep+=step
+            iteratorCount++
+            return result    
+        }
+        else{
+            return {value : undefined , done : true}
+        }
+    }
+    } 
+}
+
+let input = "10:5:2"
+let num = input.split(":")
+let start = Number(num[0])
+let numOfSteps = Number(num[1])
+let steps = Number(num[2])
+
+const iterator = makeIterator(start,numOfSteps,steps)
+let result = iterator.next()
+while(!result.done){
+    console.log(result.value)
+    result = iterator.next()
+}
+
